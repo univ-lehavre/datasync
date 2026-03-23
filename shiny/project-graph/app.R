8ac1f8e56b58a17c065c4f397e8a153d3c7b96c2
Pierre-Olivier Chasset <pierre-olivier@chasset.net>
2026-03-23 09:48:20 +0100

    Disable Topic LDA (papers) by default

--- shiny/project-graph/app.R
+++ shiny/project-graph/app.R
@@ -535,7 +535,7 @@ ui <- fluidPage(
       checkboxGroupInput("show_types",
         label = "Types de nœuds",
         choices = setNames(names(type_labels), type_labels),
-        selected = setdiff(names(type_labels), c("institution", "cptmp", "eunicoast", "hub"))
+        selected = setdiff(names(type_labels), c("institution", "cptmp", "eunicoast", "hub", "lda"))
       ),
       selectInput("filter_chercheur",
         label = "Chercheur",
